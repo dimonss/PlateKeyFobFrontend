8ddcc3ea08880d9025956832bb802f5d39d78a38
Dmitrii <faimos69@gmail.com>
2026-08-04 21:35:45 +0600

    Update Vite configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === 'serve' ? '/dev/' : '/keychain/',
   plugins: [react()],
   server: {
+    host: true,
     port: 8090,
+    allowedHosts: true,
     proxy: {
       '/api': {
         target: 'http://localhost:8095',
@@ -12,4 +15,4 @@ export default defineConfig({
       },
     },
   },
-});
+}));
